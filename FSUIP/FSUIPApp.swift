4a6b05a96855ec07ecd3f940f22c453f9b68c88d
Arthur <2272867@gmail.com>
2022-07-28 19:25:20 +0300

    1st
    1st

---
+++ FSUIP/FSUIPApp.swift
@@ -0,0 +1,20 @@
+//
+//  FSUIPApp.swift
+//  FSUIP
+//
+//  Created by Солодкий Артур Жанович on 13.07.22.
+//
+
+import SwiftUI
+
+@main
+struct FSUIPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
